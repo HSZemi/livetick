ad17e76799196350cf3bf624468aa0228de97d69
Sven Zemanek <hszemi@example.com>
2013-07-24 15:05:18 +0200

    install angepasst

--- admin/install.php
+++ admin/install.php
@@ -21,19 +21,24 @@
     
         $conn = db_connect();
         
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+        
         
     
     
         // install db
+        echo 'Erstelle Tabelle für Einträge <br />';
         $query = "CREATE TABLE ".PREFIX."entries (
             ID              int         AUTO_INCREMENT PRIMARY KEY,
             timestamp       timestamp   DEFAULT NOW(),
             content         text,
-            user            int         FOREIGN KEY REFERENCES ".PREFIX."users(ID)
+            user            int         REFERENCES ".PREFIX."users(ID)
         );";
         
         $result = mysql_query($query) or die("Anfrage fehlgeschlagen: " . mysql_error());
         
+        echo 'Erstelle Tabelle für Benutzer <br />';
         $query = "CREATE TABLE ".PREFIX."users (
             ID              int           AUTO_INCREMENT PRIMARY KEY,
             username        VARCHAR(255)  UNIQUE,
@@ -41,6 +46,10 @@
         );";
         
         $result = mysql_query($query) or die("Anfrage fehlgeschlagen: " . mysql_error());
+        
+        echo 'Erstelle Benutzer '. $user .' <br />';
+        
+        create_or_update_user($user, $pass);
         
         echo 'Installation abgeschlossen.<br /> Sie können die Seite nun schließen.';
 
@@ -67,7 +76,14 @@
         fwrite($file, $filecontent);
         
         fclose($file);
-        header("Refresh: 3; url=$PHP_SELF");
+        ?>
+        <form method="post" action="install.php">
+        <input type="hidden" name="user" value="<?php echo $_POST['user']; ?>">
+        <input type="hidden" name="pass" value="<?php echo $_POST['pass']; ?>">
+        
+        <button class="btn">Weiter</button>
+        </form>
+        <?php
         } else {
             echo 'Fehler beim Öffnen der Datei! Eventuell fehlende Schreibrechte.';
         }
@@ -79,19 +95,32 @@
         echo $_POST['basedir'] . '<br />';
         echo $_POST['passsalt'] . '<br />';
         echo $_POST['event'] . '<br />';
+        echo $_POST['user'] . '<br />';
         
         echo '<br />Installation wird gestartet...';
     } else {
     ?>
         
-        <form method="post" action="./install_db.php">
+        <form method="post" action="install.php">
             <p>Benutzername für Datenbank<br /><input name="dbuser" type="text" size="30" maxlength="120" value="dbuser" /></p>
             <p>Passwort für Datenbank<br /><input name="dbpass" type="text" size="30" maxlength="120" value="dbpass" /></p>
             <p>Name der Datenbank<br /><input name="dbname" type="text" size="30" maxlength="120" value="livetickdb" /></p>
             <p>Datenbank-Präfix<br /><input name="dbprefix" type="text" size="30" maxlength="120" value="livetick_" /></p>
             <p>Verzeichnis auf Webserver<br /><input name="basedir" type="text" size="30" maxlength="120" value="/livetick" /></p>
             <p>Passwort-Salt<br /><input name="passsalt" type="text" size="30" maxlength="120" value="d78ea1ad4c75af2123391fce7da0c374" /></p>
-            <p>Event<br /><input name="passsalt" type="text" size="30" maxlength="120" value="SP-Sitzung vom 01.01.1970" /></p>
+            <p>Event<br /><input name="event" type="text" size="30" maxlength="120" value="SP-Sitzung vom 01.01.1970" /></p>
+            <div class="control-group">
+                <label class="control-label" for="inputUser">Benutzername</label>
+                <div class="controls">
+                    <input type="text" id="inputUser" name="user" placeholder="Benutzername">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="inputPassword">Passwort</label>
+                <div class="controls">
+                    <input type="password" id="inputPassword" name="pass" placeholder="Passwort">
+                </div>
+            </div>
             <button class="btn">Erstellen</button>
         </form>
         
