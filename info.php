aaf8290cca7be3123dd9f421f51d40f0f2d2268c
Sven Zemanek <hszemi@example.com>
2013-07-26 01:05:49 +0200

    aufgeräumt

--- info.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $datum = getdate();
-    echo "Heute ist der ".$datum['mday'].".".$datum['mon'].".".$datum['year'].". Es ist ".$datum['hours'].":".$datum['minutes'].":".$datum['seconds']." Uhr. ";
-?>
